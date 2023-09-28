1526ae78712c31ae70b9d0507bb56aac86c16bc6
huawei <headcrabj1@gmail.com>
2023-09-28 09:00:29 +0500

    rewrite env library

--- main.go
+++ main.go
@@ -2,28 +2,27 @@ package env
 
 import (
 	"github.com/joho/godotenv"
-	"log"
-	"os"
 	"strconv"
 	"strings"
 )
 
+var env = make(map[string]string)
+
 func Init() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("error loading env:", err)
+	var err error
+
+	env, err = godotenv.Read()
+	if err != nil {
+		panic("error loading env: " + err.Error())
 	}
 }
 
 func GetString(key string) string {
-	return os.Getenv(key)
-}
-
-func GetSlice(key string) []string {
-	return strings.Split(os.Getenv(key), ",")
+	return env[key]
 }
 
 func GetInt(key string) int {
-	value, err := strconv.Atoi(os.Getenv(key))
+	value, err := strconv.Atoi(env[key])
 	if err != nil {
 		return 0
 	}
@@ -31,8 +30,18 @@ func GetInt(key string) int {
 	return value
 }
 
+func GetSlice(key string) []string {
+	value := env[key]
+
+	if value == "" {
+		return []string{}
+	}
+
+	return strings.Split(value, ",")
+}
+
 func GetBool(key string) bool {
-	value, err := strconv.ParseBool(os.Getenv(key))
+	value, err := strconv.ParseBool(env[key])
 	if err != nil {
 		return false
 	}
--- main_test.go
+++ main_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestEnv(t *testing.T) {
+func TestGetString(t *testing.T) {
 	Init()
 
 	gotStr := GetString("STR")
@@ -14,6 +14,10 @@ func TestEnv(t *testing.T) {
 	if gotStr != wantStr {
 		t.Errorf("got %q, wanted %q", gotStr, wantStr)
 	}
+}
+
+func TestGetInt(t *testing.T) {
+	Init()
 
 	gotInt := GetInt("INT")
 	wantInt := 100
@@ -21,6 +25,10 @@ func TestEnv(t *testing.T) {
 	if gotInt != wantInt {
 		t.Errorf("got %q, wanted %q", gotInt, wantInt)
 	}
+}
+
+func TestGetSlice(t *testing.T) {
+	Init()
 
 	gotSlice := GetSlice("SLICE")
 	wantSlice := []string{"el1", "el2", "el3"}
@@ -28,10 +36,57 @@ func TestEnv(t *testing.T) {
 	if slices.Compare(gotSlice, wantSlice) != 0 {
 		t.Errorf("got %q, wanted %q", gotSlice, wantSlice)
 	}
+}
+
+func TestGetBool(t *testing.T) {
+	Init()
 
 	gotBool := GetBool("BOOL")
 
 	if gotBool != true {
 		t.Errorf("got %t, wanted %t", gotBool, true)
+	}
+}
+
+func TestEmptyString(t *testing.T) {
+	Init()
+
+	gotEmptyString := GetString("NOT EXIST KEY")
+	wantEmptyString := ""
+
+	if gotEmptyString != wantEmptyString {
+		t.Errorf("got %q, wanted %q", gotEmptyString, wantEmptyString)
+	}
+}
+
+func TestEmptyInt(t *testing.T) {
+	Init()
+
+	gotInt := GetInt("NOT EXIST KEY")
+	wantInt := 0
+
+	if gotInt != wantInt {
+		t.Errorf("got %q, wanted %q", gotInt, wantInt)
+	}
+}
+
+func TestEmptySlice(t *testing.T) {
+	Init()
+
+	gotSlice := GetSlice("NOT EXIST KEY")
+	var wantSlice []string
+
+	if slices.Compare(gotSlice, wantSlice) != 0 {
+		t.Errorf("got %q, wanted %q", gotSlice, wantSlice)
+	}
+}
+
+func TestEmptyBool(t *testing.T) {
+	Init()
+
+	gotBool := GetBool("NOT EXIST KEY")
+
+	if gotBool != false {
+		t.Errorf("got %t, wanted %t", gotBool, false)
 	}
 }
