5636379d8c2e43398ed36c3587d47d00bb1d58df
Danil <headcrabj1@gmail.com>
2023-11-13 23:03:06 +0500

    migrate to viper

--- main.go
+++ main.go
@@ -1,41 +1,33 @@
 package env
 
 import (
-	"github.com/joho/godotenv"
-	"strconv"
+	"errors"
+	"github.com/spf13/viper"
 	"strings"
 )
 
-var env map[string]string
-
-func get(key string) string {
-	if env == nil {
-		e, err := godotenv.Read()
-		if err != nil {
-			panic("error loading env: " + err.Error())
+func init() {
+	viper.AutomaticEnv()
+	viper.AllowEmptyEnv(true)
+	viper.SetConfigFile(".env")
+	if err := viper.ReadInConfig(); err != nil {
+		var configFileNotFoundError viper.ConfigFileNotFoundError
+		if errors.As(err, &configFileNotFoundError) {
+			panic(err)
 		}
-
-		env = e
 	}
-
-	return env[key]
 }
 
 func GetString(key string) string {
-	return get(key)
+	return viper.GetString(key)
 }
 
 func GetInt(key string) int {
-	value, err := strconv.Atoi(get(key))
-	if err != nil {
-		return 0
-	}
-
-	return value
+	return viper.GetInt(key)
 }
 
 func GetSlice(key string) []string {
-	value := get(key)
+	value := viper.GetString(key)
 	if value == "" {
 		return []string{}
 	}
@@ -44,10 +36,5 @@ func GetSlice(key string) []string {
 }
 
 func GetBool(key string) bool {
-	value, err := strconv.ParseBool(env[key])
-	if err != nil {
-		return false
-	}
-
-	return value
+	return viper.GetBool(key)
 }
